5c90caafa2536ebd5c6155f78b89916ff80732d6
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-11-07 23:25:52 +0900

    Add samples of basic syntax

--- test/helloR.r
+++ test/helloR.r
@@ -6,4 +6,18 @@ func <- function(n) {
   return(n + 1)
 }
 
-checkEquals(func(1), 2)
+test.func <- function() {
+  checkEquals(func(1), 2)
+  checkEquals(func(10), 11)
+}
+
+# combine
+nums <- c(1, 2, 3, 4, 5)
+
+# matrix
+matrix(c(1, 2, 3, 10, 20, 30), 3, 2)
+
+# function
+func <- function(n) {
+  n + 1
+}
